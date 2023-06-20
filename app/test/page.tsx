5fc6a048db659f7d5449c9068f0f768298b3ccab
Mohamed Nehad <mohammednehad450@gmail.com>
2023-06-20 11:15:22 +0300

    add assetPrefix & a tag

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,13 +1,23 @@
 "use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import getConfig from "next/config";
 
 export default function Test() {
   const router = useRouter();
+  const config = getConfig();
   return (
-    <main className="p-8 text-2xl">
+    <main className="p-8 text-2xl flex flex-col gap-4">
       <Link href={"/"}>Home</Link>
+      <Link href={config.assetPrefix + "/"}>Home (with assetPrefix)</Link>
       <button onClick={() => router.push("/")}>Home (with useRouter)</button>
+      <button onClick={() => router.push(config.assetPrefix + "/")}>
+        Home (with useRouter with assetPrefix)
+      </button>
+      <a href="/">Home {"(with <a> tag)"}</a>
+      <a href={config.assetPrefix + "/"}>
+        Home {"(with <a> tag) and assetPrefix"}
+      </a>
     </main>
   );
 }
