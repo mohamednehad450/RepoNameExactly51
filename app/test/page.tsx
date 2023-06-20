c7dd03a487b1103536bb96ecf3e4f2d47ccbbab6
Mohamed Nehad <mohammednehad450@gmail.com>
2023-06-20 11:08:11 +0300

    Add with router

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,13 @@
+"use client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Test() {
+  const router = useRouter();
   return (
     <main className="p-8 text-2xl">
       <Link href={"/"}>Home</Link>
+      <button onClick={() => router.push("/")}>Home (with useRouter)</button>
     </main>
   );
 }
