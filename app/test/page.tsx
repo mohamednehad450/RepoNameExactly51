5fec61ea2081385c5ad6d7dcb74e224cfdfd2940
Mohamed Nehad <mohammednehad450@gmail.com>
2023-06-20 11:20:38 +0300

    fix prefix

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,23 +1,20 @@
 "use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import getConfig from "next/config";
-const config = getConfig();
+const prefix = "https://mohamednehad450.github.io/test-next-link";
 
 export default function Test() {
   const router = useRouter();
   return (
     <main className="p-8 text-2xl flex flex-col gap-4">
       <Link href={"/"}>Home</Link>
-      <Link href={config.assetPrefix + "/"}>Home (with assetPrefix)</Link>
+      <Link href={prefix + "/"}>Home (with assetPrefix)</Link>
       <button onClick={() => router.push("/")}>Home (with useRouter)</button>
-      <button onClick={() => router.push(config.assetPrefix + "/")}>
+      <button onClick={() => router.push(prefix + "/")}>
         Home (with useRouter with assetPrefix)
       </button>
       <a href="/">Home {"(with <a> tag)"}</a>
-      <a href={config.assetPrefix + "/"}>
-        Home {"(with <a> tag) and assetPrefix"}
-      </a>
+      <a href={prefix + "/"}>Home {"(with <a> tag) and assetPrefix"}</a>
     </main>
   );
 }
