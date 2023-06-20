f722a2527702a3f1f165bc58d9c730bf231a63c0
Mohamed Nehad <mohammednehad450@gmail.com>
2023-06-20 11:03:51 +0300

    Add assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  assetPrefix: "https://mohamednehad450.github.io/test-next-link",
 };
 
 module.exports = nextConfig;
