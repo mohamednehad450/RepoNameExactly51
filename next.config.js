29d002cb4ccfadaa2757d5e5aec7806ce26204d1
Mohamed Nehad <mohammednehad450@gmail.com>
2023-06-20 11:24:43 +0300

    remove assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  assetPrefix: "https://mohamednehad450.github.io/test-next-link",
 };
 
 module.exports = nextConfig;
